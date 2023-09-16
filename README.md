fb11641f0e81080ed38e9584b733ef428ff66aae
yoshidayu <>
2023-09-16 16:41:02 +0900

    chakra-uiの導入と動作確認を完了

--- README.md
+++ README.md
@@ -13,4 +13,16 @@ Redis 7.2.1
 
 PHPUnit
 
+react 18.2.0
+
+react-dom 18.2.0
+
+react-router-dom 6.16.0
+
+vite 4.0.0
+
+typescript
+
+chakra-ui/react 2.8.1,
+
 Docker/Docker-compose
--- resources/ts/app.tsx
+++ resources/ts/app.tsx
@@ -1,34 +1,49 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { ChakraProvider } from '@chakra-ui/react'
+import { extendTheme } from '@chakra-ui/react'
 
 import { TopPage } from "./components/TopPage";
 import { MyPage } from "./components/MyPage";
 import { PostPage } from "./components/PostPage";
 
+const theme = extendTheme({
+styles: {
+    global: {
+    body: {
+        backgroundColor: "red.100",
+        color: "red.800"
+    }
+    }
+}
+});
+
 const App = () => {
     let title: string = "Hello TypeScript React";
     return (
         <div id="main">
-            <BrowserRouter>
-                <Routes>
-                    <Route path="/" element={<TopPage />} />
-                    <Route path="/mypage" element={<MyPage />} />
-                    <Route path="/posts" element={<PostPage />} />
-                </Routes>
-                <h1>{title}</h1>
-                <ul>
-                    <li>
-                        <Link to="/">Top</Link>
-                    </li>
-                    <li>
-                        <Link to="/mypage">My</Link>
-                    </li>
-                    <li>
-                        <Link to="/posts">Post</Link>
-                    </li>
-                </ul>
-            </BrowserRouter>
+            <ChakraProvider theme={theme}>
+                <BrowserRouter>
+                    <Routes>
+                        <Route path="/" element={<TopPage />} />
+                        <Route path="/mypage" element={<MyPage />} />
+                        <Route path="/posts" element={<PostPage />} />
+                    </Routes>
+                    <h1>{title}</h1>
+                    <ul>
+                        <li>
+                            <Link to="/">Top</Link>
+                        </li>
+                        <li>
+                            <Link to="/mypage">My</Link>
+                        </li>
+                        <li>
+                            <Link to="/posts">Post</Link>
+                        </li>
+                    </ul>
+                </BrowserRouter>
+            </ChakraProvider>
         </div>
     );
 };
