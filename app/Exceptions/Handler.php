60a91b6528c4e685d83a8561d8b32e4074525415
yoshidayu <>
2024-03-26 00:15:37 +0900

    [update]APIエラーの発生時にJSONを返すように修正

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,7 +2,11 @@
 
 namespace App\Exceptions;
 
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Validation\ValidationException;
+use Illuminate\Support\Facades\Log;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -21,10 +25,58 @@ class Handler extends ExceptionHandler
     /**
      * Register the exception handling callbacks for the application.
      */
-    public function register(): void
+    public function register()
     {
-        $this->reportable(function (Throwable $e) {
-            //
-        });
+    }
+
+    /**
+     * Render an exception into an HTTP response.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Throwable  $exception
+     * @return \Illuminate\Http\Response
+     */
+    public function render($request, Throwable $exception)
+    {
+        // APIリクエストの場合のみカスタムレスポンスを返す
+        if ($request->is('api/*')) {
+            // バリデーションエラーの場合
+            if ($exception instanceof ValidationException) {
+                return response()->json([
+                    'message' => 'Validation errors',
+                    'errors' => $exception->validator->getMessageBag()
+                ], 422);
+            }
+
+            // リソースが見つからない場合
+            if ($exception instanceof NotFoundHttpException) {
+                return response()->json([
+                    'message' => $exception->getMessage(),
+                    'errors' => $exception->getMessage()
+                ], 404);
+            }
+
+            if ($exception instanceof ModelNotFoundException) {
+                return response()->json([
+                    'message' => 'resource not found',
+                    'errors' => $exception->getMessage()
+                ], 404);
+            }
+
+            if (method_exists($exception, 'getStatusCode')) {
+                return response()->json([
+                    'message' => 'an error occured',
+                    'errors' => $exception->getMessage()
+                ], $exception->getStatusCode());
+            }
+
+            // その他の例外
+            return response()->json([
+                'message' => 'an error occured',
+                'errors' => $exception->getMessage()
+            ], 500);
+        }
+
+        return parent::render($request, $exception);
     }
 }
